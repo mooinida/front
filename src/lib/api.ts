60bebe54a00787e93dde9176cb6c7354295b2344
mooinida <120772200+mooinida@users.noreply.github.com>
2025-06-05 19:56:37 +0900

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export async function fetchAnswer(query: string){
+export async function fetchAnswer(_: string){
     const res=await fetch('/api/qa?question=#{encodeURIComponent(query)}');
     if(!res.ok) throw new Error("서버 응답 오류");
     const data =await res.json();
